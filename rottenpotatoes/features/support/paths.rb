162ed6cf7aa07132280cde3fdc4735e70161f2fa
elvischen.aws <elvis.hongjie.chen@tamu.edu>
2020-10-09 20:25:48 +0000

    Done

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -14,12 +14,19 @@ module NavigationHelpers
     case page_name
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the movies page$/ then '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
+    when /^the edit page for "(.*)"$/i
+      edit_movie_path(Movie.find_by_title($1))
+    when /^the details page for "(.*)"$/i
+      movie_path(Movie.find_by_title($1))
+    when /^the Similar Movies page for "(.*)"$/i
+      similar_movie_path(Movie.find_by_title($1))
 
     else
       begin
